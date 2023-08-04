59ab323c6fad0fc1c8fd537721d24aa0736989c4
Vy Nguyen <vydpnguyen@gmail.com>
2023-08-05 01:02:18 +0700

    Initialize project with starter code

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import React from 'react';
+import { Score } from './features/score/Score.js';
+import { Board } from './features/board/Board.js';
+// Add import statements below
+
+
+
+const App = () => {
+  // Add dispatch variable below
+  
+
+  const startGameHandler = () => {
+    // Add action dispatch below
+    
+  };
+
+  const tryAgainHandler = () => {
+    // Add action dispatch below
+    
+  };
+
+  return (
+    <div className="App">
+      <Score />
+      <Board />
+      <footer className="footer">
+        <button onClick={startGameHandler} className="start-button">
+          Start Game
+        </button>
+        <button onClick={tryAgainHandler} className="try-new-pair-button">
+          Try New Pair
+        </button>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+import { store } from './app/store.js';
+// Add import statement below
+
+
+ReactDOM.render(
+  // Implement Provider component with store below
+  
+    <App />,
+  document.getElementById('root')
+);
